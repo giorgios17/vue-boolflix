c7d908e99af83d819b92032d08696e0b1ca3a250
giorgios17 <giosur@live.it>
2022-04-20 13:03:29 +0200

    header component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    MainComponent,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/style/reset";
+@import "~bootstrap";
+@import "@/assets/style/variables";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <div class="col-12 bg-dark">
+      <div
+        class="
+          container-fluid
+          d-flex
+          justify-content-between
+          align-items-center
+        "
+      >
+        <h1>{{ mainTitle }}</h1>
+        <div>
+          <input class="me-3" type="text" />
+          <button type="button" class="btn btn-primary btn-sm">Cerca!</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      mainTitle: "Boolfix",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+}
+</style>
